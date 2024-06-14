95bb9e7e488def2f24e9283b385f456eb67b0231
NGWolf2 <138962137+NGWolf2@users.noreply.github.com>
2024-06-14 00:34:25 -0400

    Update pause.js

--- commands/pause.js
+++ commands/pause.js
@@ -1,29 +1,9 @@
-/*
-
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
 const db = require("../mongoDB");
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "pause",
-  description: "Stops playing the currently playing music.",
+  description: "A músicaque esta tocando, para.",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
@@ -32,7 +12,7 @@ module.exports = {
 
     try {
       if (!queue || !queue.playing) {
-        return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+        return interaction.reply({ content: '⚠️ Não tem nenhuma música tocando!!', ephemeral: true });
       }
 
       const success = queue.pause();
@@ -40,11 +20,11 @@ module.exports = {
       const embed = new EmbedBuilder()
         .setColor('#7645fe') 
         .setAuthor({
-          name: 'Song Paused',
+          name: 'Música pausada',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157296313013117049/8061-purple-pause-icon.png?ex=651817ae&is=6516c62e&hm=4596c9fab9d8b66de8b5215b2750572ced352eed67440a1134550b846b5693b9&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-        .setDescription(success ? '**The music has been Paused for a moment!!**' : '❌ Command Error: Unable to pause song')
+        .setDescription(success ? '**The music has been Paused for a moment!!**' : '❌ Erro no comando: Não foi possivel pausar a música')
         
 
       return interaction.reply({ embeds: [embed] });
@@ -53,24 +33,3 @@ module.exports = {
     }
   },
 };
-
-/*
-
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
