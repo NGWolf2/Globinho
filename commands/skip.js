0e22eec6ca726e60caaf3bb62648feecaa055c5d
NGWolf2 <138962137+NGWolf2@users.noreply.github.com>
2024-06-14 00:01:22 -0400

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -5,8 +5,8 @@ module.exports = {
   description: "Pula pra proxima música.",
   permissions: "0x0000000000000800",
   options: [{
-    name: "number",
-    description: "mention how many songs you wanna skip",
+    name: "quantas",
+    description: "Quantas músicas você quer pular?",
     type: ApplicationCommandOptionType.Number,
     required: false
   }],
@@ -16,21 +16,21 @@ module.exports = {
     try {
 
       const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true }).catch(e => { })
+      if (!queue || !queue.playing) return interaction.reply({ content: '⚠️ Nenhuma música tocando!!', ephemeral: true }).catch(e => { })
 
       let number = interaction.options.getNumber('number');
       if (number) {
-        if (!queue.songs.length > number) return interaction.reply({ content: '⚠️ Exceeded current no of songs', ephemeral: true }).catch(e => { })
-        if (isNaN(number)) return interaction.reply({ content: '⚠️ Invalid Number', ephemeral: true }).catch(e => { })
-        if (1 > number) return interaction.reply({ content: '⚠️ Invalid Number', ephemeral: true }).catch(e => { })
+        if (!queue.songs.length > number) return interaction.reply({ content: '⚠️ Ultrapassa o numero de músicas na fila!!', ephemeral: true }).catch(e => { })
+        if (isNaN(number)) return interaction.reply({ content: '⚠️ Numero invalido', ephemeral: true }).catch(e => { })
+        if (1 > number) return interaction.reply({ content: '⚠️ Numero invalido', ephemeral: true }).catch(e => { })
 
         try {
         let old = queue.songs[0];
         await client.player.jump(interaction, number).then(song => {
-          return interaction.reply({ content: `⏯️ Skipped : **${old.name}**` }).catch(e => { })
+          return interaction.reply({ content: `⏯️ Pulado : **${old.name}**` }).catch(e => { })
         })
       } catch(e){
-        return interaction.reply({ content: '❌ Queue is empty!!', ephemeral: true }).catch(e => { })
+        return interaction.reply({ content: '❌ Não tem nenhuma música na fila!!', ephemeral: true }).catch(e => { })
       }
       } else {
 try {
@@ -49,12 +49,12 @@ try {
       iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157269773118357604/giphy.gif?ex=6517fef6&is=6516ad76&hm=f106480f7d017a07f75d543cf545bbea01e9cf53ebd42020bd3b90a14004398e&',
       url: 'https://discord.gg/FUEHs7RCqz'
     })
-    .setDescription(success ? ` **SKIPPED** : **${old.name}**` : '❌ Queue is empty!')
+    .setDescription(success ? ` **PULADO** : **${old.name}**` : '❌ Não tem nenhuma música na fila!!')
     .setTimestamp();
 
   return interaction.reply({ embeds: [embed] });
 }catch (e) {
-          return interaction.reply({ content: '❌ Queue is empty!!', ephemeral: true }).catch(e => { })
+          return interaction.reply({ content: '❌ Não tem nenhuma música na fila!!', ephemeral: true }).catch(e => { })
         }
       }
 
