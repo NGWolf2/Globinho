dbf76d0274afa7e3692f52b81ee2079b3826bfd2
NGWolf2 <138962137+NGWolf2@users.noreply.github.com>
2024-06-14 00:04:37 -0400

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -2,8 +2,8 @@ const db = require("../mongoDB");
 const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
-  name: "stop",
-  description: "Stops the music.",
+  name: "parar",
+  description: "Para a música.",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
@@ -11,7 +11,7 @@ module.exports = {
     try {
       const queue = client.player.getQueue(interaction.guild.id);
       if (!queue || !queue.playing) {
-        return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+        return interaction.reply({ content: '⚠️ Não tem música tocando!!', ephemeral: true });
       }
 
       queue.stop(interaction.guild.id);
@@ -19,11 +19,10 @@ module.exports = {
       const embed = new EmbedBuilder()
         .setColor('#f1002c')
         .setAuthor({
-          name: 'Music Stopped',
+          name: 'A música foi parada',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157305318255116400/pngtree-vector-stop-icon-png-image_4233262.jpg?ex=65182011&is=6516ce91&hm=d5a8ca6010716bae836b025f8d36557a95f14c13a705f65eb09a54161649c795&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-        .setDescription('**The journey stops, but the rhythm lives on.**')
         
 
       return interaction.reply({ embeds: [embed] });
@@ -32,25 +31,3 @@ module.exports = {
     }
   },
 };
-
-
-/*
-
-  ________.__                        _____.___.___________
- /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
-/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
-\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
- \______  /____(____  /\___  >___  >  / ______|  |____|   
-        \/          \/     \/    \/   \/                  
-
-╔════════════════════════════════════════════════════════════════════════╗
-║                                                                        ║
-║  ## Created by GlaceYT!                                                ║
-║  ## Feel free to utilize any portion of the code                       ║
-║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
-║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
-║                                                                        ║
-╚════════════════════════════════════════════════════════════════════════╝
-
-
-*/
